b23235ce14ad311ceb503cf26a254cbf32358f48
Priyanshu-Khare-codes <priyanshukhare1312@gmail.com>
2025-01-06 14:34:37 +0530

    Add sessions directory and configure file-based session storage

--- attendance/settings.py
+++ attendance/settings.py
@@ -130,3 +130,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.file'
+SESSION_FILE_PATH = os.path.join(BASE_DIR, 'sessions')
